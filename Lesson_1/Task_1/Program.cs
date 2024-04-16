8077657c2193fea2cbdb696b0c93dcef812064d4
Vlad <vladzverev21@yandex.ru>
2024-04-16 22:35:25 +0300

    Добавил кода в Lesson_1\Task_1

--- Lesson_1/Task_1/Program.cs
+++ Lesson_1/Task_1/Program.cs
@@ -1,1 +1,10 @@
 ﻿Console.WriteLine("Hello World");
+
+int value = 100;
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(value);
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
