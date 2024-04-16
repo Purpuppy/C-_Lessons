16ca2127f3b959b659bf52460ece265d79c015cc
Vlad <vladzverev21@yandex.ru>
2024-04-16 22:57:21 +0300

    Простые математические операции

---
+++ Lesson_1/Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 5;
+int b = 2;
+int sum = a + b;
+Console.WriteLine(sum);
+
+long a_1 = 4;
+long sum_2 = a + a_1;
+Console.WriteLine(sum_2);
+
+double a_3 = 2.6;
+double sum_3 = a + a_3;
+Console.WriteLine(sum_3);
+
+int sum_4 = a * b;
+long sum_5 = b * a_1;
+double sum_6 = b * a_3;
+Console.WriteLine(sum_4);
+Console.WriteLine(sum_5);
+Console.WriteLine(sum_6);
