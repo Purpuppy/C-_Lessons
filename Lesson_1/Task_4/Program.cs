d0883b29fee8edc8b82cbcb3de471d71027bdb6e
Vlad <vladzverev21@yandex.ru>
2024-04-16 23:58:36 +0300

    add Task_4

---
+++ Lesson_1/Task_4/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 10;
+int b = 20;
+int c = 25;
+int d = 15;
+int e = 8;
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
+
